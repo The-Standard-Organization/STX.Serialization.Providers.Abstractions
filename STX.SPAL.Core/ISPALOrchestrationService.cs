60050aefc1f7059a16ae7061bc4f40357fa0ca03
Raúl LB <rlorenb@gmail.com>
2024-02-04 19:47:16 +0100

    DESIGN -> POC Implementation #10

---
+++ STX.SPAL.Core/ISPALOrchestrationService.cs
@@ -0,0 +1,11 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+namespace STX.SPAL.Core
+	{
+	public interface ISPALOrchestrationService
+		{
+		T GetImplementation<T>();
+		}
+	}
---
+++ STX.Serialization.POC/Program.cs
@@ -0,0 +1,33 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Extensions.DependencyInjection;
+using STX.Serialization.Providers.Abstractions;
+using STX.SPAL.Core;
+
+namespace STX.Serialization.POC
+	{
+	internal class Program
+		{
+		static void Main(string[] args)
+			{
+			var services = new ServiceCollection();
+			services = SPALOrchestrationService.RegisterAllImplementations<ISerializationProvider>(services);
+			services
+				.AddScoped<ISPALOrchestrationService, SPALOrchestrationService>();
+
+			IServiceProvider serviceProvider = services.BuildServiceProvider();
+			using IServiceScope scope = serviceProvider.CreateScope();
+
+			ISPALOrchestrationService spalOrchestrationService =
+				scope.ServiceProvider
+					.GetRequiredService<ISPALOrchestrationService>();
+
+			ISerializationProvider serializationProvider =
+				spalOrchestrationService.GetImplementation<ISerializationProvider>();
+
+			System.Console.WriteLine(serializationProvider.GetName());
+			}
+		}
+	}
