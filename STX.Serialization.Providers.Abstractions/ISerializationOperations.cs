2d34d89d3993301624f6f25aa57632939aa38c14
Christo du Toit <christo@dutoit.co.uk>
2024-04-09 21:12:22 +0100

    CODE RUB: Removed type restriction on ISerializationOperations

--- STX.Serialization.Providers.Abstractions/ISerializationOperations.cs
+++ STX.Serialization.Providers.Abstractions/ISerializationOperations.cs
@@ -2,18 +2,14 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
-using System.IO;
 using System.Threading.Tasks;
-using STX.Serialization.Providers.Abstractions.Models;
 
 namespace STX.Serialization.Providers.Abstractions
 {
     public interface ISerializationOperations
     {
-        ValueTask<TOutput> Serialize<TInput, TOutput>(TInput @object)
-            where TOutput : IDataRepresentation<string>, IDataRepresentation<byte[]>, IDataRepresentation<Stream>;
+        ValueTask<TOutput> SerializeAsync<TInput, TOutput>(TInput @object);
 
-        ValueTask<TOutput> Deserialize<TInput, TOutput>(TInput json)
-            where TInput : IDataRepresentation<string>, IDataRepresentation<byte[]>, IDataRepresentation<Stream>;
+        ValueTask<TOutput> DeserializeAsync<TInput, TOutput>(TInput json);
     }
 }
